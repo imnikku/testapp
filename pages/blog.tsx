03b44082d584bfdb74d94e091d0f1d1f5c2a2d3e
imnikku <imnikku11@gmail.com>
2023-07-09 15:21:32 +0530

    add

---
+++ pages/blog.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+const blog = () => {
+    return (
+        <div>
+
+            <Link href='blog/nitesh'>Click</Link>
+
+        </div>
+    )
+}
+
+blog.propTypes = {}
+
+export default blog
